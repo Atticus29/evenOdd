45b3fcc55d380f3197380f17c3127c5d10d63494
Mark <mark.aaron.fisher@gmail.com>
2017-03-12 21:40:07 -0700

    add isEven function

--- evenOdd.java
+++ evenOdd.java
@@ -5,12 +5,18 @@ public class evenOdd {
     Console theConsole = System.console();
     System.out.println("Please enter a number");
     String userNumberStr = theConsole.readLine();
-    for (int i = 0; i< Integer.parseInt(userNumberStr); i++){
-      if (i%2 == 0){
+    for (int i = 0; i<= Integer.parseInt(userNumberStr); i++){
+      if (isEven(i)){
         System.out.println("Even");
       } else{
         System.out.println("Odd");
       }
     }
+  }
+  public static boolean isEven(Integer number){
+    if(number % 2 == 0){
+      return true;
+    } else
+    return false;
   }
 }
